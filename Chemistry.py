70a9142b12a1bfa8ce9e32636176594314df6bea
Sense Bar-B <sisekobabi7@gmail.com>
2026-08-20 16:49:25 +0200

    Update Chemistry.py

--- Chemistry.py
+++ Chemistry.py
@@ -1,1 +1,24 @@
 #This is a chemistry app#
+
+print("welcome to the LAB! DON'T GET BURNT!")
+
+#element data base#
+#Non-metals#
+
+elements = {
+ "H": {
+   "name": "Hydrogen",
+   "atomic_number": 1,
+   "atomic_mass": 1.008,
+   "group":"Group1(non-metal)",
+   "period":1,
+   "density": "0.000084 g/cubiic-cm",
+   "melting_point": "-259.1 degrees-C",
+   "boiling_point": "-252.9 degrees-C", 
+ }
+}
+symbol=input("Enter element symbol:")
+ if symbol in elements:
+  print(elements[name])
+   else:
+    print("Element not found"),
